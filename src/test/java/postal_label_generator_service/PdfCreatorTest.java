691a4bf327b6bc18e49eb233265f7db542d3818a
b-franka <b.franku@gmail.com>
2017-01-10 14:49:49 +0100

    empty methods for pdf generator testing

---
+++ src/test/java/postal_label_generator_service/PdfCreatorTest.java
@@ -0,0 +1,18 @@
+package postal_label_generator_service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PdfCreatorTest {
+    @Test
+    public void convertPostalDataToPdf() throws Exception {
+
+    }
+
+    @Test
+    public void convertToBytes() throws Exception {
+
+    }
+
+}
