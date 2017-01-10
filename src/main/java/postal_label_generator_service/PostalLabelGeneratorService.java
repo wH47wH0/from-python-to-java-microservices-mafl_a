4c1c6f2a1e99d74f79f65bc7bf2fa80ed1e1473a
wH47wH0 <csepella.miki@gmail.com>
2017-01-10 11:40:49 +0100

    basic webserver up and running, added dependency for logging

--- src/main/java/postal_label_generator_service/PostalLabelGeneratorService.java
+++ src/main/java/postal_label_generator_service/PostalLabelGeneratorService.java
@@ -1,8 +1,15 @@
 package postal_label_generator_service;
 
+import static spark.Spark.*;
+
 /**
  * Created by kekesaron on 2017.01.10..
  */
 public class PostalLabelGeneratorService {
-
+    public static void main(String[] args) {
+        get("/", (request, response) -> {
+            response.redirect("http://images-cdn.9gag.com/photo/24566_700b.jpg");
+            return null;
+        });
+    }
 }
