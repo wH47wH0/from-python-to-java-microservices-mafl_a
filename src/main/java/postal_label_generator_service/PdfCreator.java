ed8d5516f23abe248033530d551ba7b711dbe5a7
b-franka <b.franku@gmail.com>
2017-01-10 14:50:24 +0100

    convert to bytes method returns with bytes as string

--- src/main/java/postal_label_generator_service/PdfCreator.java
+++ src/main/java/postal_label_generator_service/PdfCreator.java
@@ -1,7 +1,7 @@
 package postal_label_generator_service;
 
+import java.io.*;
 import java.util.List;
-import java.io.IOException;
 import java.util.UUID;
 
 import org.apache.pdfbox.exceptions.COSVisitorException;
@@ -10,9 +10,7 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
-/**
- * Created by kekesaron on 2017.01.10..
- */
+
 public class PdfCreator {
     public static String convertPostalDataToPdf(List<String> postalData){
         try{
@@ -42,5 +40,26 @@ public class PdfCreator {
             System.out.println(e.getMessage());
         }
         return "";
+    }
+
+    public static String convertToBytes(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+
+        FileInputStream fis = new FileInputStream(file);
+        //System.out.println(file.exists() + "!!");
+        //InputStream in = resource.openStream();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        try {
+            for (int readNum; (readNum = fis.read(buf)) != -1; ) {
+                bos.write(buf, 0, readNum); //no doubt here is 0
+                //Writes len bytes from the specified byte array starting at offset off to this byte array output stream.
+                System.out.println("read " + readNum + " bytes,");
+            }
+        } catch (IOException ex) {
+            System.out.println("jaj");
+        }
+        byte[] bytes = bos.toByteArray();
+        return new String(bytes);
     }
 }
