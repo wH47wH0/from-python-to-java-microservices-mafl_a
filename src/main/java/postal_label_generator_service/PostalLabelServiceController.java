8ad259d5fb72f2c4665ad122ebc6e502dd8f8404
b-franka <b.franku@gmail.com>
2017-01-10 14:50:49 +0100

    called pdf methods

--- src/main/java/postal_label_generator_service/PostalLabelServiceController.java
+++ src/main/java/postal_label_generator_service/PostalLabelServiceController.java
@@ -1,28 +1,43 @@
 package postal_label_generator_service;
 
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import spark.Response;
 import spark.Request;
 import spark.Response;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by kekesaron on 2017.01.10..
- */
+
 public class PostalLabelServiceController {
-    public void getPostalData(Request request, Response response) throws JSONException {
-        JSONObject json = new JSONObject(request.body().toString());
+
+    private static PostalLabelServiceController INSTANCE;
+
+    public static PostalLabelServiceController getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PostalLabelServiceController();
+        }
+        return INSTANCE;
+    }
+
+    private PostalLabelServiceController() {}
+
+    public String getPostalData(Request request, Response response) throws JSONException, FileNotFoundException {
+        String body = request.body();
+        JSONObject json = new JSONObject(response.body());
         List<String> postalData = new ArrayList<>();
         postalData.add(json.getString("name"));
         postalData.add(json.getString("country"));
         postalData.add(json.getString("city"));
         postalData.add(json.getString("address"));
         postalData.add(json.getString("zipcode"));
-//        convertPostalDataToPdf(postalData);
 
-
-
+        response.header("Content-Type", "application/pdf");
+        String fileName = PdfCreator.convertPostalDataToPdf(postalData);
+        return PdfCreator.convertToBytes(fileName);
     }
+
 }
