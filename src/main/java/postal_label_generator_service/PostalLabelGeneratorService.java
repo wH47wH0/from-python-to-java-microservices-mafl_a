3992d161da7c0bbee01f180be020da9a1d31cbb7
Áron Kékes <kekes.aron@gmail.com>
2017-01-11 11:14:19 +0100

    Add Spark debug and route

--- src/main/java/postal_label_generator_service/PostalLabelGeneratorService.java
+++ src/main/java/postal_label_generator_service/PostalLabelGeneratorService.java
@@ -1,15 +1,14 @@
 package postal_label_generator_service;
 
 import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
 
 /**
  * Created by kekesaron on 2017.01.10..
  */
 public class PostalLabelGeneratorService {
     public static void main(String[] args) {
-        get("/", (request, response) -> {
-            response.redirect("http://images-cdn.9gag.com/photo/24566_700b.jpg");
-            return null;
-        });
+        post("/", PostalLabelServiceController::getPostalData);
+        enableDebugScreen();
     }
 }
