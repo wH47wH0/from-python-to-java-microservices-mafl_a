bd7f9e763a9fc56ec92b83d8e7addc8dd2099691
wH47wH0 <csepella.miki@gmail.com>
2017-01-18 23:51:08 +0100

    created javadoc comments for the whole package

--- src/main/java/postal_label_generator_service/PostalLabelGeneratorService.java
+++ src/main/java/postal_label_generator_service/PostalLabelGeneratorService.java
@@ -4,8 +4,15 @@ import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
 
 /**
- * Created by kekesaron on 2017.01.10..
+ * This class contains the main method which purpose is to set up a Spark web server as the one API endpoint for a
+ * postal label microservice most practical to utilize for any kind of web store.
+ * The idea is when a customer of a webshop places an order the client (the store itself) calls the API
+ * with the name and shipping address and immediately gets back a PDF document with the same details in a format
+ * convenient to print out and use as a postal label for shipping.
+ *
+ * @see <a href="http://sparkjava.com/">Spark</a>
  */
+
 public class PostalLabelGeneratorService {
     public static void main(String[] args) {
         post("/", PostalLabelServiceController.getInstance()::getPostalData);
--- src/main/java/postal_label_generator_service/PostalLabelServiceController.java
+++ src/main/java/postal_label_generator_service/PostalLabelServiceController.java
@@ -3,8 +3,9 @@ package postal_label_generator_service;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import spark.Response;
 import spark.Request;
+import spark.Response;
+
 import javax.servlet.ServletOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,6 +13,13 @@ import java.util.List;
 
 import static postal_label_generator_service.PdfCreator.convertPostalDataToPdf;
 
+/**
+ * This class contains the {@link #getPostalData} method for a postal label microservice
+ * most practical to utilize for any kind of web store.
+ * The idea is when a customer of a webshop places an order the client (the store itself) calls the API
+ * with the name and shipping address and immediately gets back a PDF document with the same details in a format
+ * convenient to print out and use as a postal label for shipping.
+ */
 
 public class PostalLabelServiceController {
 
@@ -25,6 +33,31 @@ public class PostalLabelServiceController {
     }
 
     private PostalLabelServiceController() {}
+
+    /**
+     * Serves a PDF to save containing the text it reads out from the request body.
+     * <p>
+     * Requests a JSONObject in the body of a POST request that has a corresponding name and shipping address as values
+     * to the following keys in that exact order:
+     * <p><ul>
+     * <li>"name"
+     * <li>"country"
+     * <li>"city"
+     * <li>"address"
+     * <li>"zipcode"
+     * </ul><p>
+     * In case the request's built up according to above rules the method responds with a PDF doc with the received
+     * postal data written in it.
+     * This app uses the spark web framework to create the API endpoints so the parameters can be found in
+     * {@link Request} and {@link Response} respectively.
+     *
+     * @param request  a spark request object which body should contain the shipping details
+     * @param response  a spark response object where the resulting PDF travels
+     * @return a status message 200 if the content of the JSON meets the requirements
+     * @throws JSONException  JSON in the request body differs from the specification
+     * @throws IOException
+     * @see <a href="http://sparkjava.com/">Spark</a>
+     */
 
     public int getPostalData(Request request, Response response) throws JSONException, IOException {
         JSONObject json = new JSONObject(request.body());
